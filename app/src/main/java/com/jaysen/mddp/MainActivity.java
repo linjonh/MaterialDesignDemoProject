82341e973e26835fa6dd31e1835e59525ab734df
linjianyou <jianyou.lin@mobimtech.com>
2015-08-18 18:03:21 +0800

    modified mainactivity as list activity to laucher detail

--- app/src/main/java/com/jaysen/mddp/MainActivity.java
+++ app/src/main/java/com/jaysen/mddp/MainActivity.java
@@ -1,38 +1,71 @@
 package com.jaysen.mddp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setListAdapter(new SimpleAdapter(this, getData(),
+                android.R.layout.simple_list_item_1, new String[]{"title"},
+                new int[]{android.R.id.text1}));
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private List<? extends Map<String, ?>> getData() {
+        ArrayList<HashMap<String, Object>> datas = new ArrayList<>();
+        Intent sampleIntent = new Intent();
+        sampleIntent.setAction(Intent.ACTION_MAIN);
+        sampleIntent.addCategory("com.jaysen.example.MDDP");
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> list = pm.queryIntentActivities(sampleIntent, 0);
+        if (list == null) {
+            return datas;
         }
+        for (ResolveInfo resolveInfo : list) {
+            CharSequence title = resolveInfo.loadLabel(pm);
+            //filter label to get wanted activity class
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("title", title.toString());
+            map.put("intent", newActivityIntent(resolveInfo.activityInfo.packageName,
+                    resolveInfo.activityInfo.name));
+            datas.add(map);
+        }
+        return datas;
+    }
 
-        return super.onOptionsItemSelected(item);
+    private Intent newActivityIntent(String pkg, String claz) {
+        Intent intent = new Intent();
+        intent.setClassName(pkg, claz);
+        return null;
+    }
+    private Intent newClusterIntent(String pkg, String claz) {
+        Intent intent = new Intent();
+        intent.setClassName(pkg, claz);
+        return null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Map<String, Object> map = (Map<String, Object>) l.getItemAtPosition(position);
+
+        Intent intent = (Intent) map.get("intent");
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/jaysen/mddp/TextInputLayoutActivity.java
@@ -0,0 +1,68 @@
+package com.jaysen.mddp;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+
+public class TextInputLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_input_layout);
+
+        final TextInputLayout textInputLayout = (TextInputLayout) findViewById(R.id.my_input_layout);
+
+        EditText editText = textInputLayout.getEditText();
+        textInputLayout.setHint("Password");
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 4) {
+                    textInputLayout.setError("Password error");
+                    textInputLayout.setErrorEnabled(true);
+                } else {
+                    textInputLayout.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_text_input_layout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
